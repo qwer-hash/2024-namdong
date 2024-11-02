7e8786d5004e7b31eba77f8b5483009b0b3b52f3
qwer-harsh <samss10402@naver.com>
2024-11-02 10:42:42 +0900

    while

---
+++ practice/06. while  for zz.py
@@ -0,0 +1,35 @@
+#while 조건문 ~일때
+# for - list랑 짝궁
+
+num = -1
+
+while num > 100 or num < 0:
+    num = int(input("수학 성적을 입력해주세요. ::"))
+
+if num > 80:
+    print("A반")
+elif num > 60:
+    print("B반")
+else :
+    print("C반")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
