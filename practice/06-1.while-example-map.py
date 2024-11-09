c5cb14b1a90102d5934a132548d6b0204b08568d
qwer-harsh <samss10402@naver.com>
2024-11-09 12:08:25 +0900

    06-1.while-example-map

--- practice/06-1.while-example-map.py
+++ practice/06-1.while-example-map.py
@@ -2,30 +2,32 @@ user_input = ""
 X = 0
 Y = 0
 
+valid_inputs = ["EXIT", "UP", "DOWN", "LEFT","RIGHT"]
+
 while user_input != "EXIT":
-    while user_input != "EXIT" and user_input != "UP" and user_input != "DOWN" and user_input != "LEFT" and user_input != "RIGHT":
+    user_input = ""  # user_input 초기화
+    while user_input not in valid_inputs:
         user_input = input("움직일 방향을 입력하세요 (종료는 EXIT) :: ")
-    print(user_input)
 
     if user_input == "UP":
         if Y < 100:
-            Y = Y + 1
+            Y = Y+1
         else:
-            print("범위를 벗어날 수 없습니다.")
+            print("map의 범위를 벗어날 수 없습니다.")
     elif user_input == "DOWN":
         if Y > 0:
-            Y = Y - 1
+            Y = Y-1
         else:
-            print("범위를 벗어날 수 없습니다.")
-    elif user_input == "LEFT":
-        if X > 0:
-            X = X - 1
+            print("map의 범위를 벗어날 수 없습니다.")
+    elif user_input =="LEFT":
+        if X>0:
+            X = X-1
         else:
-            print("범위를 벗어날 수 없습니다.")
+            print("map의 범위를 벗어날 수 없습니다.")
     elif user_input == "RIGHT":
         if X < 100:
-            X = X + 1
+            X = X+1
         else:
-            print("범위를 벗어날 수 없습니다.")
-    user_input = ""
+            print("map의 범위를 벗어날 수 없습니다.")
+    print("현재 위치 : (",X,",",Y,")" )
 print("프로그램을 종료합니다.")
