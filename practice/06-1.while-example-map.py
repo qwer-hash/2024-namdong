a8055e6ad5b6994376eb3f3d8cbe167b961fc84c
qwer-harsh <samss10402@naver.com>
2024-11-09 12:05:53 +0900

    06-1.while-example-map

--- practice/06-1.while-example-map.py
+++ practice/06-1.while-example-map.py
@@ -1,13 +1,31 @@
-x, y = 0, 0
+user_input = ""
+X = 0
+Y = 0
 
-def move(direction);
-    global x, y
+while user_input != "EXIT":
+    while user_input != "EXIT" and user_input != "UP" and user_input != "DOWN" and user_input != "LEFT" and user_input != "RIGHT":
+        user_input = input("움직일 방향을 입력하세요 (종료는 EXIT) :: ")
+    print(user_input)
 
-    if direction == 'up':
-            y += 1
-    elif direction == 'down':
-            y -= 1
-    elif direction == 'left':
-            x -= 1
-    elif direction == 'right':
-            x += 1
+    if user_input == "UP":
+        if Y < 100:
+            Y = Y + 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    elif user_input == "DOWN":
+        if Y > 0:
+            Y = Y - 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    elif user_input == "LEFT":
+        if X > 0:
+            X = X - 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    elif user_input == "RIGHT":
+        if X < 100:
+            X = X + 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    user_input = ""
+print("프로그램을 종료합니다.")
