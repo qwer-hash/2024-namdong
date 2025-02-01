2dcf42164b7ec2c046b1516f16c9f8bb4ec24363
User <samss10402@naver.com>
2025-02-01 11:54:30 +0900

    변경

---
+++ practice/dd.py
@@ -0,0 +1,27 @@
+from practice.game import user_input
+
+vm = {"콜라": 1000, "사이다": 1200, "과자": 1500, "물": 800}
+bal = 10000
+
+while True:
+    mode = input("\n1. 사용자 2. 관리자 3. 종료\n선택: ")
+
+    if mode == '1':  # 사용자
+       print(f"{item}: {price}원")
+        user input = input("물건: ")
+
+        if user_input ==
+        bal -= vm[choice]
+        print("구매 완료. 잔액: {bal}원")
+        else:
+        print("잘못된 선택")
+
+
+    elif mode == '2':  # 관리자
+        print("관리자 모드")
+
+    elif mode == '3':  # 종료
+        break
+
+    else:
+        print("잘못된 선택.")
